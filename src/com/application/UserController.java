f63d0e14ab004ab87af50724099ef1223376a77b
Laila <laila.zahran@stud.fci-cu.edu.eg>
2016-03-27 22:20:11 +0200

    FrontEnd Functions

--- src/com/application/UserController.java
+++ src/com/application/UserController.java
@@ -46,6 +46,34 @@ public class UserController {
 	public Response showLocationPage(){
 		return Response.ok(new Viewable("/ShowLocation.jsp")).build();
 	}
+	
+	@GET
+	@Path("/followUser")
+	@Produces(MediaType.TEXT_HTML)
+	public Response showFollowerse(){
+		return Response.ok(new Viewable("/followUser.jsp")).build();
+	}
+	
+	@GET
+	@Path("/unFollowUser")
+	@Produces(MediaType.TEXT_HTML)
+	public Response showUnFollowers(){
+		return Response.ok(new Viewable("/unFollowUser.jsp")).build();
+	}
+	
+	@GET
+	@Path("/getFollowerPosition")
+	@Produces(MediaType.TEXT_HTML)
+	public Response showFollowingList(){
+		return Response.ok(new Viewable("/getFollowerPosition.jsp")).build();
+	}
+	
+	@GET
+	@Path("/getFollowers")
+	@Produces(MediaType.TEXT_HTML)
+	public Response showFollowerPosition(){
+		return Response.ok(new Viewable("/getFollowers.jsp")).build();
+	}
 
 	@POST
 	@Path("/updateMyLocation")
@@ -53,8 +81,8 @@ public class UserController {
 	public String updateLocation(@FormParam("lat") String lat, @FormParam("long") String lon){
 		HttpSession session = request.getSession();
 		Long id = (Long) session.getAttribute("id");
-		//String serviceUrl = "http://se2firstapp-softwareeng2.rhcloud.com/FCISquare/rest/updatePosition";
-		String serviceUrl = "http://localhost:8080/FCISquare/rest/login";
+		String serviceUrl = "http://firstapp-sw2projectfci.rhcloud.com/FCISquare/rest/updatePosition";
+		//String serviceUrl = "http://localhost:8080/FCISquare/rest/login";
 
 		String urlParameters = "id=" + id + "&lat=" + lat + "&long="+ lon;
 		// System.out.println(urlParameters);
@@ -82,8 +110,8 @@ public class UserController {
 	@Produces(MediaType.TEXT_HTML)
 	public Response showHomePage(@FormParam("email") String email,
 			@FormParam("pass") String pass) {
-		//String serviceUrl = "http://se2firstapp-softwareeng2.rhcloud.com/FCISquare/rest/login";
-		String serviceUrl = "http://localhost:8080/FCISquare/rest/login";
+		String serviceUrl = "http://firstapp-sw2projectfci.rhcloud.com/FCISquare/rest/login";
+		//String serviceUrl = "http://localhost:8080/FCISquare/rest/login";
 
 		String urlParameters = "email=" + email + "&pass=" + pass;
 		// System.out.println(urlParameters);
@@ -120,8 +148,8 @@ public class UserController {
 	@Produces(MediaType.TEXT_HTML)
 	public Response showHomePage(@FormParam("name") String name,
 			@FormParam("email") String email, @FormParam("pass") String pass) {
-		//String serviceUrl = "http://se2firstapp-softwareeng2.rhcloud.com/FCISquare/rest/signup";
-		String serviceUrl = "http://localhost:8080/FCISquare/rest/signup";
+		String serviceUrl = "http://firstapp-sw2projectfci.rhcloud.com/FCISquare/rest/signup";
+		//String serviceUrl = "http://localhost:8080/FCISquare/rest/signup";
 
 		String urlParameters = "name=" + name + "&email=" + email + "&pass="
 				+ pass;
@@ -153,4 +181,137 @@ public class UserController {
 		return null;
 
 	}
+	
+	
+	
+	@POST
+	@Path("/dofollowUser")
+    @Produces(MediaType.TEXT_HTML)
+	public String showFollowers(@FormParam("followerid") String id1,
+			@FormParam("followingid") String id2) {
+		
+		String serviceUrl = "http://firstapp-sw2projectfci.rhcloud.com/FCISquare/rest/followUser";
+		//String serviceUrl = "http://localhost:8080/FCISquare/rest/followUser";
+
+	   String urlParameters = "followerid=" + id1 + "&followingid=" + id2;
+		// System.out.println(urlParameters);
+		String retJson = Connection.connect(serviceUrl, urlParameters, "POST",
+				"application/x-www-form-urlencoded;charset=UTF-8");
+		HttpSession session = request.getSession();
+		JSONObject obj = new JSONObject();
+		JSONParser parser = new JSONParser();
+		try {
+			obj = (JSONObject)parser.parse(retJson);
+			Long check = (Long) obj.get("check");
+			if(check == 1)
+				return "Your Follow is Done";
+			else
+				return "A problem occured";
+
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "A problem occured";
+
+	}
+    
+	////////////////unfollowUser//////////////////
+	@POST
+	@Path("/dounFollowUser")
+	//@Produces(MediaType.TEXT_HTML)
+	public String showUnFollowers(@FormParam("followerid") String id1,
+			@FormParam("followingid") String id2) {
+		String serviceUrl = "http://firstapp-sw2projectfci.rhcloud.com/FCISquare/rest/unFollowUser";
+		//String serviceUrl = "http://localhost:8080/FCISquare/rest/unFollowUser";
+
+		String urlParameters = "followerid=" + id1 + "&followingid=" + id2;
+		// System.out.println(urlParameters);
+		String retJson = Connection.connect(serviceUrl, urlParameters, "POST",
+				"application/x-www-form-urlencoded;charset=UTF-8");
+		HttpSession session = request.getSession();
+		JSONObject obj = new JSONObject();
+		JSONParser parser = new JSONParser();
+		try {
+			obj = (JSONObject)parser.parse(retJson);
+			Long check = (Long) obj.get("check");
+			if(check == 1)
+				return "Your unFollow is Done";
+			else
+				return "A problem occured";
+
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "A problem occured";
+
+	}
+	/////////////getfollowers///////////////
+	@POST
+	@Path("/dogetFollowers")
+	//@Produces(MediaType.TEXT_HTML)
+	public Response showFollowingList(@FormParam("id") String id1) {
+		String serviceUrl = "http://firstapp-sw2projectfci.rhcloud.com/FCISquare/rest/getFollowers";
+		//String serviceUrl = "http://localhost:8080/FCISquare/rest/getFollowers";
+
+		String urlParameters = "followerid=" + id1 ;
+		String retJson = Connection.connect(serviceUrl, urlParameters, "POST",
+				"application/x-www-form-urlencoded;charset=UTF-8");
+		HttpSession session = request.getSession();
+		JSONObject obj = new JSONObject();
+		JSONParser parser = new JSONParser();
+		try {
+			obj = (JSONObject) parser.parse(retJson);
+			
+			session.setAttribute("name", obj.get("name"));
+			session.setAttribute("id", obj.get("id"));
+			
+			Map<String, String> map = new HashMap<String, String>();
+
+			map.put("id", (String) obj.get("id"));
+			map.put("name", (String) obj.get("name"));
+
+			return Response.ok(new Viewable("/home.jsp", map)).build();
+
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+/////////////getFollowerPosition///////////////
+
+@POST
+@Path("/dogetFollowerPosition")
+@Produces(MediaType.TEXT_HTML)
+public Response showFollowerPosition(@FormParam("id") String id1) {
+String serviceUrl = "http://firstapp-sw2projectfci.rhcloud.com/FCISquare/rest/getFollowerPosition";
+//String serviceUrl = "http://localhost:8080/FCISquare/rest/getFollowerPosition";
+
+String urlParameters = "followerid=" + id1;
+String retJson = Connection.connect(serviceUrl, urlParameters, "POST", "application/x-www-form-urlencoded;charset=UTF-8");
+HttpSession session = request.getSession();
+JSONObject obj = new JSONObject();
+JSONParser parser = new JSONParser();
+try {
+    obj = (JSONObject) parser.parse(retJson);
+    session.setAttribute("lat", obj.get("lat"));
+	session.setAttribute("long", obj.get("long"));
+    Map<String, String> map = new HashMap<String,String>();
+
+    map.put("lat", (String) obj.get("lat"));
+    map.put("long", (String) obj.get("long"));
+
+    return Response.ok(new Viewable("/home.jsp", map)).build();
+
+} catch (ParseException e) {
+    // TODO Auto-generated catch block
+    e.printStackTrace();
+}
+return null;
+
+}
+
 }
